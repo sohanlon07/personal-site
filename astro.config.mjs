cdfa857e243962da1d14d5f90df6ea1289174618
Shane OHanlon <sohanlon07@gmail.com>
2025-10-26 22:16:07 +0000

    feat: Add sitemaps and robot.txts generation files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://shaneohanlon.dev",
+  integrations: [sitemap()],
+});
